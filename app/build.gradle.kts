8bd6c92f9d9b9ae4f889424bcbd2a4b8fb30e90a
mustafaDalal <mustafadalal.md@gmsil.com>
2025-10-18 13:18:56 +0530

    -bumped version to 1.0(3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.md.mypuzzleapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
